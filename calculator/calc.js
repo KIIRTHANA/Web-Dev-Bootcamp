c9bbd0aac4c103beb8ffe1131b746c088486b688
kirthanaa <kirthana.s@btech.christuniversity.in>
2021-05-14 23:08:53 +0530

    Add styles.css

--- calculator/calc.js
+++ calculator/calc.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.get("/", function (req, res) {
     //console.log(__dirname);
     res.sendFile(__dirname + "/index.html");

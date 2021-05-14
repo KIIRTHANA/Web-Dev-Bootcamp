fca34f5308191e06cd2c6ae1909e8f788e5efd78
kirthanaa <kirthana.s@btech.christuniversity.in>
2021-05-15 00:18:56 +0530

    Add views header and footer

--- ToDo-v1/app.js
+++ ToDo-v1/app.js
@@ -5,11 +5,13 @@ const { urlencoded } = require('body-parser');
 const app = express();
 
 let items = ["Web Dev", "GCR work", "Internship Work"];
+let workItems = ["task1"];
+let route = "";
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
     let today = new Date();
     let options = {
@@ -19,16 +21,34 @@ app.get("/", function (req, res) {
     };
     let day = today.toLocaleDateString("en-US", options);
     res.render("list", {
-        kindofDay: day,
+        listTitle: day,
         newListItems: items,
+        route: "/"
     });
 });
 
 app.post("/", function (req, res) {
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
+
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work",
+        newListItems: workItems,
+        route: "/work"
+    })
+});
+app.get("/about", function (req, res) {
+    res.render("about");
+})
+
 
 app.listen(3000, function () {
     console.log("server is running...");
